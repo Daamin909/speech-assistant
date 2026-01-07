2758c931c245a6f51eaf3a507aeb130c65e4b8dd
Daamin909 <ashai.daamin@gmail.com>
2026-01-07 23:03:59 +0530

    added button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+"use client";
+
 import { Navbar01 } from "@/components/ui/shadcn-io/navbar-01";
+import { Button } from "@/components/ui/button";
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [hasStarted, setHasStarted] = useState(false);
+  const onClick = () => {
+    console.log("hello");
+    setHasStarted(true);
+    audioRef.current?.play();
+  };
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
+      <audio src="/start_sound.mp3" className="" ref={audioRef} />
+      {!hasStarted && (
+        <Button
+          size={"lg"}
+          className="text-7xl w-fit h-fit py-5 px-7"
+          onClick={onClick}
+        >
+          Start
+        </Button>
+      )}
     </div>
   );
 }
